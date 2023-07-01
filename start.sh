ef3963349739b9167fed1237406b23d22b46d811
root <root@kleanup.de>
2023-07-02 00:58:43 +0200

    reworked start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-# Activate virtual environment (if applicable)
-source venv/Scripts/activate
-
-# Start uWSGI server
-uwsgi --http :8080 --wsgi-file app.py \
-      --callable app_name \
-      --master \
-      --processes 4 \
-      --threads 2 
+cd $(dirname "$0")
+source ./venv/bin/activate
+uwsgi --socket 0.0.0.0:7997 --protocol=http -w wsgi
